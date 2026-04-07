3203128e314dd0458973dc3c08b72b5e7cc256e9
alpomishmamanazarov <alpomishmamanazarov04@gmail.com>
2026-04-07 16:41:31 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,15 @@
+import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+# Render uchun soxta port ochish
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running!")
+
+port = int(os.environ.get("PORT", 8080))
+HTTPServer(('0.0.0.0', port), SimpleHandler
 import yt_dlp
 import os
 import uuid
